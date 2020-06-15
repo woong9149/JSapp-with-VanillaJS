3814248988ed826c8cfd57f579d7365483e1ddf3
woong9149 <sewoong0409@naver.com>
2020-06-15 23:50:44 +0900

    username localStorage에 저장

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser",
     SHOWING_ON = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_ON);
+    form.addEventListener("submit",handleSubmit);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_ON)
     greeting.classList.add(SHOWING_ON);
@@ -14,13 +30,13 @@ function paintGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-        
+        askForName()
     }else{
         paintGreeting(currentUser);
     }
 }
 
 function init(){
-
+    loadName();
 }
 init();
