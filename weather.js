30a1c9b84a1dee9acd5aedac175a6a81115aee68
woong9149 <sewoong0409@naver.com>
2020-06-18 19:57:50 +0900

    위치기반 날씨씨api 적용

---
+++ weather.js
@@ -0,0 +1,54 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = '241051bf13976dd3ddf8b8d9f247255e';
+const COORDS = 'coords';
+
+function getWeather(lat,lng){
+ fetch(
+     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+     ).then(function(response){
+         return response.json();
+     }).then(function(json){
+         const temperature = json.main.temp;
+         const place = json.name; 
+         weather.innerText = `${temperature} @ ${place}`;
+     })
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS,JSON.stringify(coordsObj));
+}
+ 
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,//latitude = latitude
+        longitude//longitude = longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log('Cant access geo location');
+}
+
+function askForCoords(){
+     navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError)
+}
+
+function loadCoords(){
+    const loadCoords = localStorage.getItem(COORDS);
+    if(loadCoords === null){
+        askForCoords();
+    }else{
+        //getWacther
+        const parseCoords = JSON.parse(loadCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
